c0da29bec987099d636cd45c26415707bed23891
WillianSantos013 <floriano.santos.willian@escola.pr.gov.br>
2026-05-18 09:23:14 -0300

    atualização 18\05

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    console.log(i);
+
+for(let j=j; j < botoes.length; j++){
+    botoes[j].classList.remove("ativo")
 }
+    botoes [i].classList.add("ativo");
+}
+    console.log();
